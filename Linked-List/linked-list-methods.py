38263480d4d4ee85dd3e6673b96e875a77a83147
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-20 05:35:43 -0500

    Update linked-list-methods.py

--- Linked-List/linked-list-methods.py
+++ Linked-List/linked-list-methods.py
@@ -1,28 +1,34 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
 #         
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+    
+    # Append: append 'value' at the end of the list
+    # time: O(n); space: O(1)
     def append(self, value):
-        '''Append a value at the end of the list'''
-        if self.head is None:
-            self.head = Node(value)
-            return
-        tail = head
-        while tail.next:
-            tail = tail.next
-        tail.next = Node(value)
-        return
-      
-    def prepend(self, value):
         if self.head is None:
             self.head = Node(value)
             return
         tail = self.head
-        self.head = Node(value)
-        self.head.next = tail
+        while tail.next:
+            tail = tail.next
+        tail.next = Node(value)
         return
+    
+    # Prepend
+    
+    # Search
+    
+    # Remove
+    
+    # Pop
+    
+    # Insert
+    
+    # Size
+    
+    # To python list
