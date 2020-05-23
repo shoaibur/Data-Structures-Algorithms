8c72d6fb46ffed4226eb05f0853a63c21613f266
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-23 00:15:52 -0500

    Update create-stack.py

--- Stacks-and-Queues/create-stack.py
+++ Stacks-and-Queues/create-stack.py
@@ -1,1 +1,17 @@
-
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+      
+    def push(self, value):
+        self.stack.append(value)
+      
+    def pop(self):
+        self.stack.pop()
+# Create a stack
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+print(s.stack) # [1, 2, 3]
+s.pop()
+print(s.tack) # [1, 2]
