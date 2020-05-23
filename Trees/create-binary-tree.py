bf8ecb8e2ca2d1702a479fdd043b7a6fbc977f80
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-22 22:58:56 -0500

    Update create-binary-tree.py

--- Trees/create-binary-tree.py
+++ Trees/create-binary-tree.py
@@ -1,1 +1,13 @@
-
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def set_value(self, value):
+        self.value = value
+    
+    def get_value(self):
+        return self.value
+      
+    
