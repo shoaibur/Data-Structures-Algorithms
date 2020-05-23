9b43695c1715954ec08a1511019a329b05225e0a
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-23 00:19:22 -0500

    Update create-queue.py

--- Stacks-and-Queues/create-queue.py
+++ Stacks-and-Queues/create-queue.py
@@ -1,1 +1,17 @@
-
+class Queue(object):
+    def __init__(self):
+        self.queue = []
+    
+    def enq(self, value):
+        self.queue.append(value)
+      
+    def deq(self):
+        self.queue.pop()
+# Create a queue
+q = Queue()
+q.enq(1)
+q.enq(2)
+q.enq(3)
+print(q.queue) # [3, 2, 1]
+q.deq()
+print(q.queue) # [3, 2]
