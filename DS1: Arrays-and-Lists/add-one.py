76743a5af684b87089aa5c0f3867530acfa01e11
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-26 21:18:42 -0500

    Update add-one.py

--- DS1: Arrays-and-Lists/add-one.py
+++ DS1: Arrays-and-Lists/add-one.py
@@ -1,1 +1,10 @@
-
+def add_one(nums):
+    carry = 1
+    for num in nums:
+        sums, carry = (num+carry)%2, (num+carry)//2
+        nums[-1] = sums
+        if carry == 0:
+            return nums
+    if carry == 1:
+        return [1] + nums
+    
