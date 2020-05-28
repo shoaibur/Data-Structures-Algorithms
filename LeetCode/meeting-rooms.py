ed69618fd21d0921934c6652c29ffd96ab173d04
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-27 23:21:40 -0500

    Update meeting-rooms.py

--- LeetCode/meeting-rooms.py
+++ LeetCode/meeting-rooms.py
@@ -1,1 +1,15 @@
-
+# Can join all meetings?
+def meeting_rooms(intervals):
+    if not intervals: return None
+    if len(intervals) == 1: return True
+    intervals.sort()
+    out = [intervals[0]]
+    for i in range(1, len(intervals)):
+        overlap = overlaps(out[-1], intervals[i])
+        if overlap: return False
+        out.append(intervals[0])
+    return True
+  
+def overlaps(x, y):
+    overlap = min(x[1],y[1]) - max(x[0],y[0]) > 0
+    return overlap
