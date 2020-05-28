d12d76b733fef703040a9ee17e23edb107b46c85
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-27 23:04:22 -0500

    Update spiral-matrix.py

--- LeetCode/spiral-matrix.py
+++ LeetCode/spiral-matrix.py
@@ -1,1 +1,17 @@
-
+def spiral_matrix(matrix):
+    if not matrix: return matrix
+    out = []
+    while True:
+        if matrix != []:
+            out += matrix.pop(0)
+        if matrix != []:
+            for i in range(len(matrix)):
+                out.append(matrix[i].pop())
+        if matrix != []:
+            out += matrix.pop()[::-1]
+        if matrix != []:
+            for i in range(len(matrix)-1,-1,-1):
+                out.append(matrix[i].pop(0))
+        if matrix == []:
+            break
+    return out
