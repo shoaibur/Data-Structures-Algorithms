fc17a0629c8999e14de843797fe0b6da50daa556
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-28 16:02:55 -0500

    Update flipping-image.py

--- LeetCode/flipping-image.py
+++ LeetCode/flipping-image.py
@@ -1,1 +1,34 @@
+def flipping_image(A): # Vertical flip
+    if not A: return A
+    i, j = 0, len(A)-1
+    while i < j:
+        A[i], A[j] = A[j], A[i]
+        i, j = i+1, j-1
+    nrow = len(A)
+    ncol = len(A[0])
+    for i in range(nrow):
+        for j in range(ncol):
+            if A[i][j] == 1:
+                A[i][j] = 0
+            else:
+                A[i][j] = 1
+    return A
 
+def flipping_image(A): # Horizontal flip  
+    A = transpose(A)
+    i, j = 0, len(A)-1
+    while i < j:
+        A[i], A[j] = A[j], A[i]
+        i, j = i+1, j-1
+    nrow = len(A)
+    ncol = len(A[0])
+    for i in range(nrow):
+        for j in range(ncol):
+            A[i][j] = 1 - A[i][i] # Flip between 0 and 1.
+    return transpose(A)
+
+def transpose(A):
+    A = list(zip(*A))
+    A = [list(A[i]) for i in range(len(A))]
+    return A
+    
