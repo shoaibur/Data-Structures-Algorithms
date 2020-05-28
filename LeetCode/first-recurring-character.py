c7878306a89d04abe11aadf54baef8fe59394e53
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-28 01:11:23 -0500

    Update first-recurring-character.py

--- LeetCode/first-recurring-character.py
+++ LeetCode/first-recurring-character.py
@@ -1,1 +1,12 @@
-
+def first_recurring_character(string):
+    if len(string) <= 1: return -1
+    d = {}
+    for i, char in enumerate(string):
+        if char in d:
+            d[char] = d[char] + [i]
+        else:
+            d[char] = [i]
+    for char in d:
+        if len(d[char]) > 1:
+            return char
+    return -1
