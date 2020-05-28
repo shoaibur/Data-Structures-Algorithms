464bc027256b9bdc55f4300c6ba5dd15a8c7826f
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-27 23:13:44 -0500

    Update merge-intervals.py

--- LeetCode/merge-intervals.py
+++ LeetCode/merge-intervals.py
@@ -1,1 +1,18 @@
+def merge_intervals(intervals):
+    if len(intervals) <= 1: return intervals
+    intervals.sort()
+    out = [intervals[0]]
+    for i in range(1, len(intervals)):
+        overlap, merged_interval = overlaps( out[-1], intervals[i] )
+        if overlap:
+            out[-1] = merged_interval
+        else:
+            out.append(intervals[i])
+    return out
 
+def overlaps(x, y):
+    merged_interval = None
+    overlap = min(x[1], y[1]) - max(x[0], y[0]) >= 0
+    if overlap:
+        merged_interval = ( min(x[0], y[0]), max(x[1], y[1]) )
+    return overlap, merged_interval
