4c265584e07782564e06d89541e9867cd9bd7c58
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-27 22:29:06 -0500

    Update even-after-odd.py

--- DS1: Arrays-and-Lists/even-after-odd.py
+++ DS1: Arrays-and-Lists/even-after-odd.py
@@ -1,1 +1,25 @@
-
+def even_after_odd(nums):
+    even = []
+    odd = []
+    for num in nums:
+        if num % 2: odd.append(num)
+        else: even.append(num)
+    return odd + even
+  
+def even_after_odd(head):
+    even = None
+    odd = None
+    tail = head
+    while tail:
+        if tail.value % 2:
+            odd = append(odd, tail.value)
+        else:
+            even = append(even, tail.value)
+        tail = tail.next
+    tail = odd
+    while tail.next:
+        tail = tail.next
+    tail.next = even
+    return odd
+  
+    
