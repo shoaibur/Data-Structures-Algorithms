f356efa160d7bebbfb93a64281ee0ac6c1076d97
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-30 10:26:26 -0500

    Update add-two-numbers.py

--- LeetCode/add-two-numbers.py
+++ LeetCode/add-two-numbers.py
@@ -49,5 +49,91 @@ def add(nums1, nums2):
     return int1 + int2
 
 # Problem 5
+# Solution 1 -- convert lls into integers, sum them and back the sum to the ll
+def add(nums1, nums2):
+    # ll to int
+    int1 = ll2int(nums1)
+    int2 = ll2int(nums2)
+    # sum
+    summ = int1 + int2
+    # Back to ll
+    head = int2ll(summ)
+    return head
 
-        
+def ll2int(head):
+    if head is None: return 0
+    tail = head
+    nums = []
+    while tail:
+        nums.append(tail.value)
+        tail = tail.next
+    nums = int( ''.join([str(num) for num in nums]) )
+    return nums
+
+def int2ll(nums):
+    nums = [int(num) for num in str(nums)]
+    head = Node(nums[0])
+    for num in nums[1:]:
+        append(head, num)
+    return head
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+def append(head, value):
+    if head is None: return Node(value)
+    tail = head
+    while tail.next:
+        tail = tail.next
+    tail.next = Node(value)
+    return head
+
+# Solution 2 -- reverse lls, sum them element-by-element and prepend the sums to a new ll
+def add(head1, head2):
+    # Equalize length
+    n1 = length(head1)
+    n2 = length(head2)
+    if n1 > n2:
+        for i in range(n1-n2):
+            head2 = prepend(head2, 0)
+    if n2 > n1:
+        for i in range(n2-n1):
+            head1 = prepend(head1, 0)
+    # Reverse ll
+    head1 = reverse(head1)
+    head2 = reverse(head2)
+    
+    # Sum and store into new ll
+    new_head = Node(None)
+    carry = 0
+    tail1 = head1
+    tail2 = head2
+    while tail1:
+        summ = tail1.value + tail2.value + carry
+        summ, carry = summ % 10, summ // 10
+        new_head = prepend(new_head, summ)
+    if carry > 0:
+        new_head = prepend(new_head, carry)
+    return new_head
+
+def reverse(head):
+    new_head = Node(None)
+    tail = head
+    while tail:
+        value = tail.value
+        new_head = prepend(new_head, value)
+    return new_head
+def prepend(head, value):
+    if head is None: return Node(value)
+    tail = head
+    head = Node(value)
+    head.next = tail
+    return head
+def length(head):
+    tail = head
+    count = 0
+    while tail:
+        count += 1
+        tail = tail.next
+    return count
