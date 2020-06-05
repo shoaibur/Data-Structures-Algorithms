20aec048bef1adc242a74efcd0ba2e07ed792015
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-05 17:41:01 -0500

    Update implement-stack-from-linked-list.py

--- DS2: Stacks-and-Queues/implement-stack-from-linked-list.py
+++ DS2: Stacks-and-Queues/implement-stack-from-linked-list.py
@@ -4,5 +4,11 @@ def stack_from_linked_list(head):
     while tail:
         stack.push(tail.value)
     tail = tail.next
-    
     return stack
+
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+        
+    def push(self, value):
+        self.stack.append(value)
