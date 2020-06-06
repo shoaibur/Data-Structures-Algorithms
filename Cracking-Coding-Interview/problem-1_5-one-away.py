af0fcdc605d77978d819cb59673c9538ec8a6055
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-06 13:23:48 -0500

    Update problem-1_5-one-away.py

--- Cracking-Coding-Interview/problem-1_5-one-away.py
+++ Cracking-Coding-Interview/problem-1_5-one-away.py
@@ -1,3 +1,12 @@
+# One Away: There are three types of edits that can be performed on strings: 
+# insert a character, remove a character, or replace a character. Given two 
+# strings, write a function to check if they are one edit (or zero edits) away.
+# EXAMPLE
+# pale, ple -> true 
+# pales, pale -> true 
+# pale, bale -> true 
+# pale, bake -> false
+
 def one_away(s1, s2):
     n1, n2 = len(s1), len(s2)
     if abs(n1 - n2) > 1: return False
