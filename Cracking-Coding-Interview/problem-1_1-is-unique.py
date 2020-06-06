68b2d9971853e1eef610bb75dc29396ae214e73b
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-06 14:13:10 -0500

    Update problem-1_1-is-unique.py

--- Cracking-Coding-Interview/problem-1_1-is-unique.py
+++ Cracking-Coding-Interview/problem-1_1-is-unique.py
@@ -1,1 +1,20 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+# What if you cannot use additional data structures?
 
+# With additional data structure
+def is_unique(s):
+    d = {}
+    # O(n), O(n)
+    for char in s:
+        d[char] = d.get(char, 0) + 1
+        if d[char] > 1:
+            return False
+    return True
+  
+# Without additional data structure
+def is_unique(s):
+    # O(n^2), O(1)
+    for i in range(len(s)-1):
+        if s[i] in s[i+1:]:
+            return False
+    return True
