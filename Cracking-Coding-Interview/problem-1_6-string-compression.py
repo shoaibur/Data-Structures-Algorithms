af0456500811c85779d2b8020ff3ae647d21a502
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-07 14:59:36 -0500

    Update problem-1_6-string-compression.py

--- Cracking-Coding-Interview/problem-1_6-string-compression.py
+++ Cracking-Coding-Interview/problem-1_6-string-compression.py
@@ -4,3 +4,17 @@
 # original string, your method should return the original string. You can assume 
 # the string has only uppercase and lowercase letters (a - z).
 
+def string_compression(s):
+    if len(s) <= 1: return s
+    out = []
+    i, j = 0, 1
+    while j < len(s):
+        if s[i] == s[j]:
+            j += 1
+        else:
+            out.append(s[i]+str(j-i))
+            i, j = j, j+1
+    out = ''.join(out)
+    if len(out) > len(s):
+        return s
+    return out
