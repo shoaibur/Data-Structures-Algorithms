220a780bca9621859a03fabef92860eaa15b0430
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-10 13:10:40 -0500

    Create az01-k-frequent-keywords.py

---
+++ Cracking-Coding-Interview/az01-k-frequent-keywords.py
@@ -0,0 +1,4 @@
+Given a list of reviews, a list of keywords and an integer k. Find the most popular k keywords in order of most to least frequently mentioned.
+The comparison of strings is case-insensitive.
+Multiple occurances of a keyword in a review should be considred as a single mention.
+If keywords are mentioned an equal number of times in reviews, sort alphabetically.
