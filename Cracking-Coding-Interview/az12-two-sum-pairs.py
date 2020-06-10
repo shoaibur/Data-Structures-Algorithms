a2ad87111019b15160ac2654a9ff51b8a27f5c3b
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-09 21:24:36 -0500

    Create az12-two-sum-pairs.py

---
+++ Cracking-Coding-Interview/az12-two-sum-pairs.py
@@ -0,0 +1,5 @@
+Given a list of positive integers nums and an int target, return indices of the two numbers such that they add up to a target - 30.
+Conditions:
+You will pick exactly 2 numbers.
+You cannot pick the same element twice.
+If you have muliple pairs, select the pair with the largest number.
