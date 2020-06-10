0688ff7157c596dd94676b58cfa724459800efe5
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-10 08:56:48 -0500

    Update 10-search-insert-position.py

--- Challenge-2020-06/10-search-insert-position.py
+++ Challenge-2020-06/10-search-insert-position.py
@@ -1,14 +1,18 @@
-def searchInsert(self, nums: List[int], target: int) -> int:
-        if not nums: return None
-        low, high = 0, len(nums)-1
-        while low <= high:
-            mid = (low+high) // 2
-            if target == nums[mid]:
-                return mid
-            elif target < nums[mid]:
-                high = mid - 1
-            else:
-                low = mid + 1
-        if target < nums[mid]:
+# Given a sorted array and a target value, return the index if the target is found. 
+# If not, return the index where it would be if it were inserted in order.
+# You may assume no duplicates in the array.
+
+def searchInsert(nums, target):
+    if not nums: return None
+    low, high = 0, len(nums)-1
+    while low <= high:
+        mid = (low+high) // 2
+        if target == nums[mid]:
             return mid
-        return low
+        elif target < nums[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    if target < nums[mid]:
+        return mid
+    return low
