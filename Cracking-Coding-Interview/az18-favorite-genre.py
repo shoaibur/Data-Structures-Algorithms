813c465913ed9b834f013592b67d36f3ea1ad1e1
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-09 22:22:05 -0500

    Create az18-favorite-genre.py

---
+++ Cracking-Coding-Interview/az18-favorite-genre.py
@@ -0,0 +1,3 @@
+Given a map Map<String, List<String>> userSongs with user names as keys and a list of all the songs that the user has listened to as values.
+Also given a map Map<String, List<String>> songGenres, with song genre as keys and a list of all the songs within that genre as values. The song can only belong to only one genre.
+The task is to return a map Map<String, List<String>>, where the key is a user name and the value is a list of the user's favorite genre(s). Favorite genre is the most listened to genre. A user can have more than one favorite genre if he/she has listened to the same number of songs per each of the genres.
