81d092b5fc4ee0103d95fecdfd13f0d9a922f3ad
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-15 07:17:23 -0500

    Update 15-search-in-binary-search-tree.py

--- Challenge-2020-06/15-search-in-binary-search-tree.py
+++ Challenge-2020-06/15-search-in-binary-search-tree.py
@@ -1,1 +1,8 @@
-
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None: return None
+        if root.val == val:
+            return root
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        elif val > root.val:
+            return self.searchBST(root.right,val)
