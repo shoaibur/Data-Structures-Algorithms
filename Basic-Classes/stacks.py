d2b8fb36f6144f5a96eab9c97c5bb46fe6fd530f
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-16 09:26:26 -0500

    Update stacks.py

--- Basic-Classes/stacks.py
+++ Basic-Classes/stacks.py
@@ -1,1 +1,18 @@
-
+class Stack:
+    def __init__(self):
+        self.stack = []
+        
+    def push(self, value): # O(1)
+        self.stack.append(value)
+        
+    def pop(self): # O(1)
+        return self.stack.pop()
+    
+# Test
+s = Stack()
+nums = [1,2,3,4]
+for num in nums:
+    s.push(num)
+print('stack:', s.stack)
+print('pop:', s.pop())
+print('stack:', s.stack)
