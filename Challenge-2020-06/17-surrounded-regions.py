190e141bbd151cbd16a86f838cb1e80c4f29942d
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-17 14:00:18 -0500

    Update 17-surrounded-regions.py

--- Challenge-2020-06/17-surrounded-regions.py
+++ Challenge-2020-06/17-surrounded-regions.py
@@ -1,2 +1,2 @@
-Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
-A region is captured by flipping all 'O's into 'X's in that surrounded region.
+# Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+# A region is captured by flipping all 'O's into 'X's in that surrounded region.
