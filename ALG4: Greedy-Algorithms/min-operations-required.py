1ac9bf1b2c5033a6fc8140819f05c1f5d160e93a
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-20 22:41:09 -0500

    Update min-operations-required.py

--- ALG4: Greedy-Algorithms/min-operations-required.py
+++ ALG4: Greedy-Algorithms/min-operations-required.py
@@ -1,1 +1,14 @@
-
+def min_operations(target):
+    """
+    Return number of steps taken to reach a target number
+    input: target number (as an integer)
+    output: number of steps (as an integer)
+    """
+    operation = 0
+    while target > 0:
+        if target % 2 == 0:
+            target = target // 2
+        else:
+            target = target - 1
+        operation += 1
+    return operation
