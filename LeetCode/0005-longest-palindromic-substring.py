d067ebad38250792740df76ca5de0ff3a55516a8
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-24 22:40:56 -0500

    Update 0005-longest-palindromic-substring.py

--- LeetCode/0005-longest-palindromic-substring.py
+++ LeetCode/0005-longest-palindromic-substring.py
@@ -1,13 +1,13 @@
-def longestPalindrome(self, s: str) -> str:
-        p = ''
-        for i in range(len(s)):
-            p = max( p, self.palindrome(s, i, i+1), self.palindrome(s, i-1, i+1), key = len )
-        return p
-    
+def longest_palindromic_substring(s):
+    p = ''
+    for i in range(len(s)):
+        p = max( p, self.palindrome(s, i, i+1), self.palindrome(s, i-1, i+1), key = len )
+    return p
+
     def palindrome(self, s, i, j):
-        while i >= 0 and j < len(s):
-            if s[i] != s[j]:
-                break
-            i -= 1
-            j += 1
-        return s[i+1:j]
+    while i >= 0 and j < len(s):
+        if s[i] != s[j]:
+            break
+        i -= 1
+        j += 1
+    return s[i+1:j]
