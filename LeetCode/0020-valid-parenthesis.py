804eaadfa7b2d56ec0f0978fe33c8e283c6986f4
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-24 23:11:33 -0500

    Update 0020-valid-parenthesis.py

--- LeetCode/0020-valid-parenthesis.py
+++ LeetCode/0020-valid-parenthesis.py
@@ -1,18 +1,18 @@
-def isValid(self, s: str) -> bool:
-        if len(s) == 0: return True
-        if len(s) % 2: return False
-        if s[0] in ')}]': return False
-        
-        maps = { '(':')', '{':'}', '[':']' }
-        
-        stack = []
-        for p in s:
-            if p in '({[':
-                stack.append(p)
-            elif len(stack) > 0:
-                popped = stack.pop()
-                if maps[popped] != p:
-                    return False
-            else:
+def valid_parenthesis(s):
+    if len(s) == 0: return True
+    if len(s) % 2: return False
+    if s[0] in ')}]': return False
+
+    maps = { '(':')', '{':'}', '[':']' }
+
+    stack = []
+    for p in s:
+        if p in '({[':
+            stack.append(p)
+        elif len(stack) > 0:
+            popped = stack.pop()
+            if maps[popped] != p:
                 return False
-        return len(stack) == 0
+        else:
+            return False
+    return len(stack) == 0
