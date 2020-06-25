fc7ac6194701c49e7684aae105d0f0e84628257b
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-24 23:10:53 -0500

    Create 0020-valid-parenthesis.py

---
+++ LeetCode/0020-valid-parenthesis.py
@@ -0,0 +1,18 @@
+def isValid(self, s: str) -> bool:
+        if len(s) == 0: return True
+        if len(s) % 2: return False
+        if s[0] in ')}]': return False
+        
+        maps = { '(':')', '{':'}', '[':']' }
+        
+        stack = []
+        for p in s:
+            if p in '({[':
+                stack.append(p)
+            elif len(stack) > 0:
+                popped = stack.pop()
+                if maps[popped] != p:
+                    return False
+            else:
+                return False
+        return len(stack) == 0
