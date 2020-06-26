f445473cfa519931af3cafba02fdd699d483ffd2
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-26 13:31:14 -0500

    Create 0240-search-a-2d-matrixi-ii.py

---
+++ LeetCode/0240-search-a-2d-matrixi-ii.py
@@ -0,0 +1,18 @@
+def searchMatrix(matrix, target):
+    """
+    :type matrix: List[List[int]]
+    :type target: int
+    :rtype: bool
+    """
+    if len(matrix) == 0: return False
+    nrow = len(matrix)
+    ncol = len(matrix[0])
+    i, j = 0, ncol-1
+    while i < nrow and j >= 0:
+        if matrix[i][j] == target:
+            return True
+        elif target < matrix[i][j]:
+            j -= 1
+        else:
+            i += 1
+    return False
