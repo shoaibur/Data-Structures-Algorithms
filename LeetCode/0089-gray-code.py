3d7ac3acbc6ae210ee5dc9b44869120cce207d0c
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-25 19:01:41 -0500

    Update 0089-gray-code.py

--- LeetCode/0089-gray-code.py
+++ LeetCode/0089-gray-code.py
@@ -1,7 +1,17 @@
-    def grayCode(self, n: int) -> List[int]:
-        # Total number of codes = 2^n = 1 << n
-        # ith gray code = i xor floor(i/2) = i ^ i >> 1
-        out = []
-        for i in range(1 << n):
-            out.append( i ^ (i >> 1) )
-        return out
+# Bit manipulation/shifting definitions:
+
+# 2 ** n == 1 << n
+
+# b multiplied by 2 == left shift by by 1 bit == b << 1
+# b multiplied by 4 == left shift by by 2 bit == b << 2
+
+# b divided by 2 == right shift by by 1 bit == b >> 1
+# b divided by 4 == right shift by by 2 bit == b >> 2
+
+def gray_code(n):
+    # Total number of codes = 2^n = 1 << n
+    # ith gray code = i xor floor(i/2) = i ^ i >> 1
+    out = []
+    for i in range(1 << n):
+        out.append( i ^ (i >> 1) )
+    return out
