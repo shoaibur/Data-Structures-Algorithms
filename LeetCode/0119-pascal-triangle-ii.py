b3cf24ed0dd56e9e1addd4ade2043f8f894a9370
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-25 20:21:16 -0500

    Create 0119-pascal-triangle-ii.py

---
+++ LeetCode/0119-pascal-triangle-ii.py
@@ -0,0 +1,16 @@
+    def getRow(self, n: int) -> List[int]:
+        if n == 0: return [1]
+        out = [1, 1]
+        for k in range(2, n+1):
+            out = [1]+[out[i]+out[i+1] for i in range(len(out)-1)]+[1]
+        return out
+        
+        ''' solution 2
+        if n == 0: return [1]
+        a = [1, 1]
+        for i in range(2, n+1):
+            for j in range(len(a)-1):
+                a[j] = a[j] + a[j+1]
+            a = [1] + a
+        return a
+        '''
