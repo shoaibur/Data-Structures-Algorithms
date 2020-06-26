9fac48c1912b65ae438bbbc78b9149d164576341
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-25 23:46:07 -0500

    Create 0215-kth-largest-element.py

---
+++ LeetCode/0215-kth-largest-element.py
@@ -0,0 +1,10 @@
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums or k < 1 or len(nums) < k: return -1
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for num in nums[k:]:
+            if num > heap[0]:
+                #heap[0] = num
+                #heapq.heapify(heap)
+                heapq.heapreplace(heap, num)
+        return heap[0]
