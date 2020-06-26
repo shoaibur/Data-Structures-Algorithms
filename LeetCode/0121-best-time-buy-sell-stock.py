639d483aefa9071591ec36cc26afca9f9600961a
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-25 20:34:48 -0500

    Update 0121-best-time-buy-sell-stock.py

--- LeetCode/0121-best-time-buy-sell-stock.py
+++ LeetCode/0121-best-time-buy-sell-stock.py
@@ -1,9 +1,9 @@
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) <= 1: return 0
-        min_price = prices[0]
-        max_profit = 0
-        for curr_price in prices[1:]:
-            curr_profit = curr_price - min_price
-            max_profit = max(max_profit, curr_profit)
-            min_price = min(min_price, curr_price)
-        return max_profit
+def maxProfit(prices):
+    if len(prices) <= 1: return 0
+    min_price = prices[0]
+    max_profit = 0
+    for curr_price in prices[1:]:
+        curr_profit = curr_price - min_price
+        max_profit = max(max_profit, curr_profit)
+        min_price = min(min_price, curr_price)
+    return max_profit
