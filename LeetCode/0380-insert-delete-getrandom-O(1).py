f62f29fc82b263ab16dcf632941e3ba19940abb0
Shoaibur Rahman <shoaibeee@gmail.com>
2020-06-26 14:29:49 -0500

    Create 0380-insert-delete-getrandom-O(1).py

---
+++ LeetCode/0380-insert-delete-getrandom-O(1).py
@@ -0,0 +1,46 @@
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hash = {}
+        self.data = []
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.hash:
+            self.hash[val] = len(self.data)
+            self.data.append(val)
+            return True
+        return False
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.hash:
+            return False
+        
+        remove_value = val
+        last_value = self.data[-1]
+        remove_index = self.hash[remove_value]
+        last_index = self.hash[last_value]
+        
+        self.hash[last_value] = remove_index
+        self.hash.pop(remove_value)
+        
+        self.data[remove_index] = last_value
+        self.data.pop()
+        return True
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.data)
