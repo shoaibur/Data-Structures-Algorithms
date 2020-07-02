97df9db27d2a74ac993afa9bdd1c2e26564fcab6
Shoaibur Rahman <shoaibeee@gmail.com>
2020-07-01 22:56:39 -0500

    Update 0138-copy-list-with-random-pointers.py

--- LeetCode/0138-copy-list-with-random-pointers.py
+++ LeetCode/0138-copy-list-with-random-pointers.py
@@ -1,1 +1,26 @@
-
+def copyRandomList(head)
+    if not head: return head
+    # create duplicates
+    tail = head
+    while tail:
+        temp = tail.next
+        tail.next = Node(tail.val)
+        tail.next.next = temp
+        tail = temp
+    # copying random pointers to duplicates
+    tail = head
+    while tail:
+        if tail.random:
+            tail.next.random = tail.random.next 
+        else:
+            tail.next.random = None
+        tail = tail.next.next
+    # Separate two linked lists
+    org = head
+    copy   = head.next
+    clone = head.next
+    while(org):
+        org = org.next.next
+        copy.next = copy.next.next if copy.next else None
+        copy = copy.next
+    return clone
