a59fb819908926f9861842c1fb756a71eb7f0f1f
Shoaibur Rahman <shoaibeee@gmail.com>
2020-07-01 22:59:56 -0500

    Update 0957-prison-cells-after-n-days.py

--- LeetCode/0957-prison-cells-after-n-days.py
+++ LeetCode/0957-prison-cells-after-n-days.py
@@ -1,1 +1,19 @@
+def prisonAfterNDays(cells, N)
+    if N == 0: return cells
+    day1cells = nextday(cells)
+    cells = day1cells
+    N -= 1
+    count = 1
+    while N > 0:
+        cells = self.nextday(cells)
+        if cells == day1cells:
+            N = N % count
+        count += 1
+        N -= 1
+    return cells
 
+def nextday(cells):
+    day = [0] * len(cells)
+    for i in range(1, len(cells)-1):
+        day[i] = 1 - cells[i-1] ^ cells[i+1]
+    return day
