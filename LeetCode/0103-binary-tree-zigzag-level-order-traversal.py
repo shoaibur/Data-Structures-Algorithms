f2ffc31870a355424a66259e152f72d0366ce0dd
Shoaibur Rahman <shoaibeee@gmail.com>
2020-07-02 00:27:06 -0500

    Update 0103-binary-tree-zigzag-level-order-traversal.py

--- LeetCode/0103-binary-tree-zigzag-level-order-traversal.py
+++ LeetCode/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,1 +1,21 @@
-
+import collections
+def zigzagLevelOrder(root)
+    if root is None: return root
+    out = []
+    q = collections.deque()
+    q.append(root)
+    while len(q) > 0:
+        size = len(q)
+        count = 0
+        temp = []
+        while count < size:
+            popped = q.popleft()
+            temp.append(popped.val)
+            count += 1
+            if popped.left:
+                q.append(popped.left)
+            if popped.right:
+                q.append(popped.right)
+        out.append(temp)
+    out = [item[::-1] if i%2 else item for i, item in enumerate(out)]
+    return out
