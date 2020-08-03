84d904980a2a15d14db601ea4332836bbff3ed23
Shoaibur Rahman <shoaibeee@gmail.com>
2020-08-03 07:50:13 -0500

    Update 03-valid-palindrome.py

--- LeetCode_challenges/2020-08/03-valid-palindrome.py
+++ LeetCode_challenges/2020-08/03-valid-palindrome.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) <= 1: return True
+        
+        s = s.lower().replace(' ', '')
+        for p in string.punctuation:
+            s = s.replace(p, '')
+        
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i, j = i + 1, j - 1
+        return True
