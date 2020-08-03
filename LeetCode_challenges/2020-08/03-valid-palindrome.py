3c2daf5ca7cc8e832dbbbf1091433aecc5da9045
Shoaibur Rahman <shoaibeee@gmail.com>
2020-08-03 07:50:40 -0500

    Update 03-valid-palindrome.py

--- LeetCode_challenges/2020-08/03-valid-palindrome.py
+++ LeetCode_challenges/2020-08/03-valid-palindrome.py
@@ -12,3 +12,21 @@ class Solution:
                 return False
             i, j = i + 1, j - 1
         return True
+
+    
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) <= 1: return True
+        
+        validchar = string.ascii_lowercase + '0123456789'
+        i, j = 0, len(s)-1
+        while i < j:
+            while i < len(s)-1 and s[i].lower() not in validchar:
+                i += 1
+            while j >= 0 and s[j].lower() not in validchar:
+                j -= 1
+            if i < j and s[i].lower() != s[j].lower():
+                return False
+            i, j = i + 1, j - 1
+            
+        return True
